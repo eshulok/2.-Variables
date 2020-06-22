d7c82a9993c00d7d523578f83e8376d59cfef879
Liz S <shulok@berkeley.edu>
2020-06-22 16:26:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Variables are like nicknames for values
+#You can assign a value to a variable
+temperature = 75
+#And then use the variable name in your code
+print(temperature)
+
+#You can change the value of the variable
+temperature = 100
+print(temperature)
+
+#You can use variables for operations
+temp_today = 85
+temp_yesterday = 79
+
+#How much warmer is it today than yesterday?
+print(temp_today - temp_yesterday)
+
+#Tomorrow will be 10 degrees warmer than today
+print(temp_today + 10)
+
+#Or if you want to save the value for tomorrow's temperature, create a new variable
+temp_tomorrow = temp_today + 10
+print(temp_tomorrow)
